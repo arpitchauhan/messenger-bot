f45001eeda6b7c1525e71107f828784384cf0a58
Arpit Chauhan <arpitchauhan@gmail.com>
2017-02-01 02:58:15 +0530

    Allow post /webhook

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,10 +1,16 @@
 class BotController < ApplicationController
-  def webhook
+
+  def get_webhook
     raise 'Token not set' if ENV['webhook_token'].blank?
     if params['hub.verify_token'] == ENV['webhook_token']
       render plain: params['hub.challenge']
     else
       render plain: 'error'
     end
+  end
+
+  def post_webhook
+    Rails.logger.info "params = #{params}"
+    render status: 500
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/webhook', to: 'bot#webhook'
+  get '/webhook', to: 'bot#get_webhook'
+  post '/webhook', to: 'bot#post_webhook'
 end
