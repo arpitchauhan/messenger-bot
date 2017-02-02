bc10002db229b1cb44ff8f8c5dc9ffdbc039ec4a
Arpit Chauhan <arpitchauhan@gmail.com>
2017-02-03 01:44:38 +0530

    add functionality to create message when receiving post from facebook

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -10,6 +10,31 @@ class BotController < ApplicationController
   end
 
   def post_webhook
+    if params[:object] == 'page'
+      params[:entry].each do |entry|
+        page = Page.find_or_create_by!(facebook_id: entry[:id])
+        entry[:messaging].each do |message|
+          sender = Profile.find_or_create_by!(facebook_id: message[:sender][:id])
+          recipient_facebook_id = message[:recipient][:id]
+          # check if page is recipient
+          if page.facebook_id == recipient_facebook_id
+            time = Utils::Facebook.convert_timestamp_to_time(message[:timestamp])
+            Message.create!(sender: sender,
+                            recipient: page,
+                            timestamp: time,
+                            text: message[:message][:text],
+                            seq: message[:message][:seq],
+                            mid: message[:message][:mid])
+          else
+            Rails.logger.warn "Page ID #{page.facebook_id} not same as recipient ID #{recipient_facebook_id}"
+          end
+        end
+      end
+    end
+  rescue => e
+    Rails.logger.error "Error occurred: #{e.class} -- #{e.message}"
+    Rails.logger.error "Backtrace: #{e.backtrace}"
+  ensure
     render status: 200
   end
 end
--- spec/requests/bots_spec.rb
+++ spec/requests/bots_spec.rb
@@ -10,4 +10,35 @@ RSpec.describe "Bots", type: :request do
       end
     end
   end
+
+  describe "post /post_webhook" do
+    before :each do
+      @payload = { "object"=>"page",
+                   "entry"=> [
+                              {
+                                "id" => "732979013420893",
+                                "time" => 1485898512693,
+                                "messaging" => [
+                                                 {
+                                                   "sender" => {"id"=>"1131196886949901"},
+                                                   "recipient"=>{"id"=>"732979013420893"},
+                                                   "timestamp"=>1485896474420,
+                                                   "message"=> {
+                                                                  "mid"=>"mid.1485896474420:60c6dc8842",
+                                                                  "seq"=>1315338,
+                                                                  "text"=>"kutie"
+                                                               }
+                                                  }
+                                                ]
+                               }
+                              ]
+                  }
+
+    end
+
+    it "creates message properly" do
+      post '/webhook', @payload
+      expect(response).to have_http_status(200)
+    end
+  end
 end
