cb0c944addd5f442c51c5cfa0f5e209b0bf3f1eb
Arpit Chauhan <arpitchauhan@gmail.com>
2017-02-03 02:07:03 +0530

    Relate Entity to sent and received messages

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -10,4 +10,6 @@
 #
 
 class Entity < ApplicationRecord
+  has_many :sent_messages, class_name: 'Message', foreign_key: :sender_id
+  has_many :received_messages, class_name: 'Message', foreign_key: :recipient_id
 end
