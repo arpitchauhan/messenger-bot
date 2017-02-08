ad5b589a0fe95bda496803451bed1a9e8b37256c
Arpit Chauhan <arpitchauhan@gmail.com>
2017-02-09 02:02:54 +0530

    Add sidekiq_options to FacebookProfileFetcher

--- app/workers/facebook_profile_fetcher.rb
+++ app/workers/facebook_profile_fetcher.rb
@@ -1,5 +1,6 @@
 class FacebookProfileFetcher
   include Sidekiq::Worker
+  sidekiq_options queue: 'facebook_profile_fetcher', retry: 2, backtrace: true
 
   def perform(profile_id)
     profile = Facebook::Profile.find(profile_id)
